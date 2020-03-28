5eed6050f1f3b195ab1e7cecbc1a32150c70ccc1
Andrew <sk1723@bk.ru>
2020-03-28 21:19:46 +0300

    Typo bintray-deploy-deb.js #2

--- bintray-deploy-deb.js
+++ bintray-deploy-deb.js
@@ -6,14 +6,14 @@
     },
 
     "version": {
-        "name": ${TRAVIS_BUILD_NUMBER},
+        "name": "${TRAVIS_BUILD_NUMBER}"
     },
 
     "files":
         [
             { 
-                "includePattern": helloworld-0.0.${TRAVIS_BUILD_NUMBER}-Linux.deb,
-                "uploadPattern": ${TRAVIS_BUILD_NUMBER},
+                "includePattern": "helloworld-0.0.${TRAVIS_BUILD_NUMBER}-Linux.deb",
+                "uploadPattern": "${TRAVIS_BUILD_NUMBER}",
                 "matrixParams": {
                     "deb_distribution": "trusty",
                     "deb_component": "main",
