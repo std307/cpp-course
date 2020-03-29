1ca05833329aa5244368df24a398f7dd50af4a12
Andrew <sk1723@bk.ru>
2020-03-29 10:21:25 +0300

    Fixed filename in desc file

--- bintray-deploy-deb.js
+++ bintray-deploy-deb.js
@@ -5,12 +5,12 @@
         "subject": "otuscourse"
     },
     "version": {
-        "name": "42"
+        "name": "43"
     },
     "files":
         [
             { 
-                "includePattern": "build/bin/(.*\\.deb)",
+                "includePattern": "(.*\\.deb)",
                 "uploadPattern": "$1",
                 "matrixParams": {
                     "deb_distribution": "trusty",
@@ -19,8 +19,8 @@
                 }
             },
             { 
-                "includePattern": "(helloworld-0.0.24-Linux.deb)",
-                "uploadPattern": "$1",
+                "includePattern": "helloworld-0.0.25-Linux.deb",
+                "uploadPattern": "helloworld-0.0.25-Linux.deb",
                 "matrixParams": {
                     "deb_distribution": "trusty",
                     "deb_component": "main",
