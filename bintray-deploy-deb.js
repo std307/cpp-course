c33cea2ec02dbba12748885e264c98c5b6989f9d
Andrew <sk1723@bk.ru>
2020-03-29 10:04:18 +0300

    Another attempt to upload deb to BinTray properly

--- bintray-deploy-deb.js
+++ bintray-deploy-deb.js
@@ -3,35 +3,29 @@
         "name": "cpp-homework1",
         "repo": "cpp-homework1",
         "subject": "otuscourse",
-        "attributes": [
-            { "name": "att1", "values" : ["val1"], "type": "string" },
-            { "name": "att2", "values" : [1, 2.2, 4], "type": "number" },
-            { "name": "att5", "values" : ["2014-12-28T19:43:37+0100"], "type": "date" }
-        ]
     },
     "version": {
         "name": "42",
-        "desc": "This is a version",
-        "released": "2015-01-04",
-        "vcs_tag": "0.5",
-        "attributes": [
-            { "name": "VerAtt1", "values" : ["VerVal1"], "type": "string" },
-            { "name": "VerAtt2", "values" : [1, 3.3, 5], "type": "number" },
-            { "name": "VerAtt3", "values" : ["2015-01-01T19:43:37+0100"], "type": "date" }
-        ]
     },
     "files":
         [
             { 
-//                "includePattern": "(.*\\.deb)",
-                "includePattern": "(.*\.deb)",
+                "includePattern": "build/bin/(.*\.deb)",
                 "uploadPattern": "$1",
                 "matrixParams": {
                     "deb_distribution": "trusty",
                     "deb_component": "main",
                     "deb_architecture": "amd64"
                 }
-            }
+            },
+            { 
+                "includePattern": "(helloworld-0.0.24-Linux.deb)",
+                "uploadPattern": "$1",
+                "matrixParams": {
+                    "deb_distribution": "trusty",
+                    "deb_component": "main",
+                    "deb_architecture": "amd64"
+                }
         ],
     "publish": true
 }
