dd1cf86fc4327361f08b089e622dd0a33f384836
Andrew <sk1723@bk.ru>
2020-03-29 09:46:40 +0300

    Experiment with version numbers

--- bintray-deploy-deb.js
+++ bintray-deploy-deb.js
@@ -2,15 +2,28 @@
     "package": {
         "name": "cpp-homework1",
         "repo": "cpp-homework1",
-        "subject": "otuscourse"
+        "subject": "otuscourse",
+        "attributes": [
+            { "name": "att1", "values" : ["val1"], "type": "string" },
+            { "name": "att2", "values" : [1, 2.2, 4], "type": "number" },
+            { "name": "att5", "values" : ["2014-12-28T19:43:37+0100"], "type": "date" }
+        ]
     },
     "version": {
-        "name": "42"
+        "name": "42",
+        "desc": "This is a version",
+        "released": "2015-01-04",
+        "vcs_tag": "0.5",
+        "attributes": [
+            { "name": "VerAtt1", "values" : ["VerVal1"], "type": "string" },
+            { "name": "VerAtt2", "values" : [1, 3.3, 5], "type": "number" },
+            { "name": "VerAtt3", "values" : ["2015-01-01T19:43:37+0100"], "type": "date" }
+        ]
     },
     "files":
         [
             { 
-                "includePattern": "(.*\.deb)",
+                "includePattern": "(.*\\.deb)",
                 "uploadPattern": "$1",
                 "matrixParams": {
                     "deb_distribution": "trusty",
