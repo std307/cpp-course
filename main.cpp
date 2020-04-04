5c95259c62999d151f2ea97262f9e8c1572dbf01
Andrew <sk1723@bk.ru>
2020-04-04 12:07:19 +0300

    cmake updates BinTray desc file. Fixed tabs->4spaces

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 int main (int argc, char *argv[])
 {
     if (argc > 1){
-	std::string arg = argv[1];
-	if (arg == "-v") {
-	   std::cout << "Version: " << version() << std::endl;
-	   return 0;
-	}
+        std::string arg = argv[1];
+        if (arg == "-v") {
+            std::cout << "Version: " << version() << std::endl;
+            return 0;
+        }
     }
 
     std::cout << "Hello, World!" << std::endl;
